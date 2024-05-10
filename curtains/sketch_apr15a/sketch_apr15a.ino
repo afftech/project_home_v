be3f260a6406bd528be5717b63bc1049d0937910
afftech <39600224+afftech@users.noreply.github.com>
2024-05-10 17:55:48 +0300

    20.6

--- curtains/sketch_apr15a/sketch_apr15a.ino
+++ curtains/sketch_apr15a/sketch_apr15a.ino
@@ -1,7 +1,9 @@
 
 HardwareSerial Serial2(PB11, PB10);
+byte Number = 1;  //номер сюда впиши
 byte command[] = { 0x55, 0x00, 0x00, 0x02, 0x00, 0x02, 0x01, 0x01, 0x9D, 0x58 };
 void setup() {
+  command[6] = Number;
   Serial.begin(9600);
   Serial2.begin(9600);
   byte len = 10;
